d5ad7c58cde8419c6c217befbba81a6ff9d5dc2a
Robert Huberdeau <rhuberdeau@gmail.com>
2011-02-16 20:35:18 -0500

    recaptcha config

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,6 @@
+Recaptcha.configure do |config|
+  config.public_key  = '6LfWyMASAAAAAD5Dn7GNvrMi6fVgZAS6IecD9kH-'
+  config.private_key = '6LfWyMASAAAAAMeOoRWag-Qef_884aCTVcMQ1Vcw'
+end
+#onfig.public_key  = '6LfWyMASAAAAAD5Dn7GNvrMi6fVgZAS6IecD9kH-'
+#config.private_key = '6LfWyMASAAAAAMeOoRWag-Qef_884aCTVcMQ1Vcw'
