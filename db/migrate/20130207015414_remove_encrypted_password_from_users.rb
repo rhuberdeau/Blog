91bca0da78029459d96bfbf4b71609f9e409863b
Robert Huberdeau <rhuberdeau@gmail.com>
2013-04-14 22:41:52 -0400

    fixing migrations

--- db/migrate/20130207015414_remove_encrypted_password_from_users.rb
+++ db/migrate/20130207015414_remove_encrypted_password_from_users.rb
@@ -2,7 +2,6 @@ class RemoveEncryptedPasswordFromUsers < ActiveRecord::Migration
   def up
     remove_column :users, :encrypted_password
     remove_column :users, :reset_password_token
-    remove_column :users, :reset_password_sent_at
     remove_column :users, :remember_created_at
     remove_column :users, :sign_in_count
     remove_column :users, :current_sign_in_at
