1cfcfcbaa9e65ca0b0e3a7b08a6caed2265516c1
Robert Huberdeau <rhuberdeau@gmail.com>
2013-06-05 22:16:51 -0400

    changing asynchronous to onclick

--- app/assets/javascripts/article.js
+++ app/assets/javascripts/article.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
   l = window.location.pathname.split('/')[2].toLowerCase();
-  $.getJSON('/related-articles/' + l, function(data) {
-  	if (data.length > 0) {
-  	  $('#related_articles').html("<h2>The complete series</h2>")
-      data.forEach(appendElement)
-    }
+  $("#related_articles").click(function(){
+  	$.getJSON('/related-articles/' + l, function(data) {
+  	  if (data.length > 0) {
+  	  	$("#related_articles").css('background-color', '#fff');
+  	    $('#related_articles').html("<h2>The complete series</h2>")
+        data.forEach(appendElement)
+      }
+    });
   });
 });
 
 function appendElement(elem) {
   $('#related_articles').append("<a href=\"/articles/" + elem.cached_slug + "\">" + elem.title + "</a>" + "<br />");
-};
+};	
