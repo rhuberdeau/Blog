77bacd10d48b4805eb8b5501337721d8b82c28cc
rhuberdeau <rhuberdeau@gmail.com>
2013-09-11 17:02:54 -0400

    integrating engine

--- db/schema.rb
+++ db/schema.rb
@@ -9,98 +9,116 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130603000433) do
+ActiveRecord::Schema.define(version: 20130911175736) do
 
-  create_table "articles", :force => true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
     t.string   "cached_slug"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.boolean  "published"
     t.integer  "user_id"
     t.text     "summary"
     t.integer  "sequence_id"
   end
 
-  add_index "articles", ["user_id"], :name => "index_articles_on_user_id"
+  add_index "articles", ["user_id"], name: "index_articles_on_user_id", using: :btree
 
-  create_table "comments", :force => true do |t|
-    t.text     "body",       :null => false
-    t.integer  "article_id", :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "blog_engine_articles", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "blog_engine_comments", force: true do |t|
+    t.text     "content"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "body",       null: false
+    t.integer  "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
 
-  create_table "roles_users", :id => false, :force => true do |t|
+  create_table "roles_users", id: false, force: true do |t|
     t.integer "role_id"
     t.integer "user_id"
   end
 
-  create_table "sequences", :force => true do |t|
+  create_table "sequences", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "slugs", :force => true do |t|
+  create_table "slugs", force: true do |t|
     t.string   "name"
     t.integer  "sluggable_id"
-    t.integer  "sequence",                     :default => 1, :null => false
-    t.string   "sluggable_type", :limit => 40
+    t.integer  "sequence",                  default: 1, null: false
+    t.string   "sluggable_type", limit: 40
     t.string   "scope"
     t.datetime "created_at"
   end
 
-  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
-  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
+  add_index "slugs", ["name", "sluggable_type", "sequence", "scope"], name: "index_slugs_on_n_s_s_and_s", unique: true, using: :btree
+  add_index "slugs", ["sluggable_id"], name: "index_slugs_on_sluggable_id", using: :btree
 
-  create_table "steps", :force => true do |t|
+  create_table "steps", force: true do |t|
     t.text     "body"
     t.integer  "position"
-    t.integer  "tutorial_id", :null => false
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.integer  "tutorial_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
-  add_index "steps", ["position"], :name => "index_steps_on_position"
-  add_index "steps", ["tutorial_id"], :name => "index_steps_on_tutorial_id"
+  add_index "steps", ["position"], name: "index_steps_on_position", using: :btree
+  add_index "steps", ["tutorial_id"], name: "index_steps_on_tutorial_id", using: :btree
 
-  create_table "taggings", :force => true do |t|
-    t.integer  "article_id", :null => false
-    t.integer  "tag_id",     :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+  create_table "taggings", force: true do |t|
+    t.integer  "article_id", null: false
+    t.integer  "tag_id",     null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "taggings", ["article_id"], :name => "index_taggings_on_article_id"
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
+  add_index "taggings", ["article_id"], name: "index_taggings_on_article_id", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "tutorials", :force => true do |t|
+  create_table "tutorials", force: true do |t|
     t.string   "name"
     t.text     "summary"
     t.string   "permalink"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "tutorials", ["name"], :name => "index_tutorials_on_name", :unique => true
-  add_index "tutorials", ["permalink"], :name => "index_tutorials_on_permalink", :unique => true
+  add_index "tutorials", ["name"], name: "index_tutorials_on_name", unique: true, using: :btree
+  add_index "tutorials", ["permalink"], name: "index_tutorials_on_permalink", unique: true, using: :btree
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "",    null: false
     t.datetime "reset_password_sent_at"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "username"
     t.text     "bio"
     t.boolean  "approved"
@@ -108,10 +126,10 @@ ActiveRecord::Schema.define(:version => 20130603000433) do
     t.string   "uid"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.boolean  "admin",                  :default => false
+    t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
