e52d5902be2202382e7ccb1142a2de24df3d2a5d
rhuberdeau <rhuberdeau@gmail.com>
2015-12-27 12:26:36 -0500

    Changing the before_filter to use devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  include SessionsHelper
   
   private
-    def authenticate_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def user_must_be_admin
-      unless current_user.admin == true
+      unless current_user.try(:admin?)
         redirect_to(articles_path)
       end
     end
