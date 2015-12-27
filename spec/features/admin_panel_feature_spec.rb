5ee2f313402e2708368190dce882421acbba4687
rhuberdeau <rhuberdeau@gmail.com>
2015-12-27 15:23:17 -0500

    Adding a very basic test to make sure the admin panel is rendering

---
+++ spec/features/admin_panel_feature_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper' 
+
+describe "Admin Panel", type: :feature do 
+  let(:user) { FactoryGirl.create :user, admin: true}
+
+  it "renders the admin panel" do 
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'Log in'
+    visit "/admin"
+
+    expect(page).to have_content "Welcome to Ember"
+  end
+end
