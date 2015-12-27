2d52833f93bc6be9bc7c2457f4dc909c3188483e
rhuberdeau <rhuberdeau@gmail.com>
2015-12-27 12:40:21 -0500

    Removing unnecessary fields

---
+++ db/migrate/20151105042935_clean_up_users.rb
@@ -0,0 +1,13 @@
+class CleanUpUsers < ActiveRecord::Migration
+  def change
+    # Dropping columns that do not belong in the user model
+    # This limits the user class to a thin active record backed model
+    # It also prevents user objects from becoming God objects
+    remove_column :users, :username, :string
+    remove_column :users, :bio, :text
+    remove_column :users, :approved, :boolean
+    remove_column :users, :admin, :boolean
+    remove_column :users, :provider, :string
+    remove_column :users, :uid, :string
+  end
+end
