1dd49aa9d44d51b4dc9e1900e7c53b7391d2dca4
rhuberdeau <rhuberdeau@gmail.com>
2016-01-11 09:06:19 -0500

    Removing comments and unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,84 +1,19 @@
 Blog::Application.routes.draw do
+  # See how all your routes lay out with "rake routes"
+  
   devise_for :users
+  root :to => 'articles#index'
+
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  match 'auth/:provider/callback', to: 'sessions#callback', via: :all
-  get '/related-articles/:id', to: 'articles#related'
+  get "admin", to: 'admin#index'
+  get "/sitemap" => "sitemap#index", :as => :sitemap, :defaults => {:format => :xml}
 
+  resources :tags
   resources :tutorials do
   	resources :steps
   end
-  resources :tags
-
-  # resources :sessions, only: [:new, :create, :destroy]
-
-  # get '/signup',  to: 'users#new'
-  # get '/signin',  to: 'sessions#new'
-  # match '/signout', to: 'sessions#destroy', via: :delete
-
-  get "admin", to: 'admin#index'
-  get "admin/show"
-  get "admin/manage_comments"
-  #match "/archive" => "articles#archive", :as => :archive
-  #match "/:year(/:month(/:day))" => "articles#archive", :constraints => { :year => /\d{4}/, :month => /\d{2}/, :day => /\d{2}/ }
-  get "/:year(/:month(/:day))" => "articles#archive", :constraints => { :year => /\d{4}/, :month => /\d{2}/, :day => /\d{2}/ }
-    
   resources :articles do
     resources :comments
   end
-
-  get "/sitemap" => "sitemap#index", :as => :sitemap, :defaults => {:format => :xml}
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-  root :to => 'articles#index'
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
 end
