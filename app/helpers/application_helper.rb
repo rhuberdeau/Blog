098f2df00b62b8096f5592cb925f431694631f56
rhuberdeau <rhuberdeau@gmail.com>
2016-02-14 15:13:54 -0500

    Removing unused helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,7 +22,7 @@ module ApplicationHelper
   	if @keywords.nil?
   		base_keywords
   	else
-  		"#{@keywords}, #{base_keywords}"
+  		"#{@keywords}"
   	end
   end
   
@@ -31,26 +31,4 @@ module ApplicationHelper
       CodeRay.scan($3, $2).div(:css => :class)  
     end  
   end 
-  
-  def nav_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'current' : ''
-
-    content_tag(:li, :class => class_name) do
-      link_to link_text, link_path
-    end
-  end
-
-  def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-danger", alert: "alert-warning", notice: "alert-info" }[flash_type] || flash_type.to_s
-  end
-
-  def flash_messages(opts = {})
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do 
-              concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-              concat message 
-            end)
-    end
-    nil
-  end
 end
