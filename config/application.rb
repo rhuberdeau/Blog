b68a50956032f7b09a258ee35406a8e7c59ff504
rhuberdeau <rhuberdeau@gmail.com>
2016-02-13 22:54:36 -0500

    Updating for 4.2

--- config/application.rb
+++ config/application.rb
@@ -29,19 +29,6 @@ module Blog
     # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
-
-    # Enable the asset pipeline
-    
-    #config.assets.precompile += ["redactor-rails/*"]
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
-    config.generators do |g|
-      g.view_specs false
-      g.helper_specs false
-	  end
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
