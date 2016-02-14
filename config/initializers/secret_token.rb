dd062f8a9723dfcd9738be142b830892a48d4f1a
rhuberdeau <rhuberdeau@gmail.com>
2016-02-13 22:56:24 -0500

    Removing for 4.2

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = 'c9c0b7d81160133cb26d20856b4cbe0c9a7a95ccea9a8ef3f842acf01bb7d1862fe704ff5c549a6b99cfe6831fcc6bc8e46a7cbf043b9d277c3c593148731fe6'
-Blog::Application.config.secret_key_base = 'c9c0b7d81160133cb26d20856b4cbe0c9a7a95ccea9a8ef3f842acf01bb7d1862fe704ff5c549a6b99cfe6831fcc6bc8e46a7cbf043b9d277c3c593148731fe6'
