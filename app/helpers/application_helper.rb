38c26f8c4eecc57a8cd02ddf920fa3a4e46dd444
rhuberdeau <rhuberdeau@gmail.com>
2016-02-24 16:07:50 -0500

    Updating code styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,7 +37,7 @@ module ApplicationHelper
   
   def coderay(text)  
     text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do  
-      CodeRay.scan($3, $2).div(:css => :class)  
-    end  
+      CodeRay.scan($3, $2).div(:css => :style)  
+    end 
   end 
 end
