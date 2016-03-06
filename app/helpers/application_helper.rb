6a643ff38fba8cbebd6de6ad2a300798bb521de8
rhuberdeau <rhuberdeau@gmail.com>
2016-03-06 00:14:12 -0500

    Using the meta_tag gem instead of rolling my own

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,40 +1,4 @@
-module ApplicationHelper
-  def full_title(page_title)
-    base_title = "Robert Huberdeau"
-    if page_title.empty?
-      base_title
-    else
-      "#{base_title} | #{page_title}"
-    end
-  end
-  
-  def metadescription
-  	base_content = "A collection of tips and tutorials to help Rails developers"
-  	if @content.nil?
-  		base_content
-  	else
-  		"#{@content}"
-  	end
-  end
-  
-  def keywords
-  	base_keywords = "software development, ruby on rails, quality assurance, ruby on rails tutorials"
-  	if @keywords.nil?
-  		base_keywords
-  	else
-  		"#{@keywords}"
-  	end
-  end
-
-  def title
-    base_title = "This is going to blow your mind"
-    if @title.nil?
-      base_title
-    else
-      "#{@title}"
-    end
-  end
-  
+module ApplicationHelper 
   def coderay(text)  
     text.gsub(/\<code( lang="(.+?)")?\>(.+?)\<\/code\>/m) do  
       CodeRay.scan($3, $2).div(:css => :style)  
