e010d7d690dbf51ede40d7c41ebd9c598f098d6e
rhuberdeau <rhuberdeau@gmail.com>
2016-07-31 21:26:29 -0400

    Adding the publish controller action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -86,6 +86,20 @@ class ArticlesController < ApplicationController
       format.xml  { head :ok }
     end
   end
+
+  def publish
+    @article = Article.find(params[:id])
+    respond_to do |format|
+      if @article.update_attributes(published: true)
+        flash[:notice] = "'#{@article.title}' was successfully published."
+        format.html { redirect_to(controller: 'admin', action: 'index') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @article.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
   
   private
   
