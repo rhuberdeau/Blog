f62043ad592a923821130a0b53f4c0ca0f4bbb47
rhuberdeau <rhuberdeau@gmail.com>
2017-03-16 18:41:09 -0400

    Fixing titles, removing unused files

--- app/helpers/clean_blogs_helper.rb
+++ app/helpers/clean_blogs_helper.rb
@@ -1,7 +1,7 @@
 module CleanBlogsHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-      base_title = "Clean Blog"
+      base_title = "Robert Huberdeau"
       if page_title.empty?
           base_title
       else
