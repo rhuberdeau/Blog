1342efae623f4825f2f44ccf00129b47ac03c4f0
rhuberdeau <rhuberdeau@gmail.com>
2017-05-22 23:38:06 -0400

    Updating the email domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'example.com',
+    domain:               'smooth-lightning-97.herokuapp.com',
     user_name:            ENV['MY_EMAIL'],
     password:             ENV['EMAIL_PASSWORD'],
     authentication:       'plain',
